7e3a1c2bf23321cd637b523a09ee2e64bad87da5
BeaconHill <theageofnewstars@gmail.com>
2015-07-16 22:54:08 -0700

    Be slightly more accepting of different bandwagon vote formats

--- scrape.py
+++ scrape.py
@@ -218,11 +218,11 @@ def process_vote_from_posts(posts, freeform=None, mergeable=None):
 		munged_author = post.author.lower().replace(' ', '')
 
 		# check for a bandwagon vote
-		text = vote_lines[0].text.lstrip('@').rstrip('.!')
+		text = vote_lines[0].text.lstrip('@ ').rstrip('.! ')
 		text = text.lower().replace(' ', '')
 		if text in last_votes_from_person and text != munged_author and \
 			vote_lines[0].type in POSITIVE_VOTE_LINE_TYPES:
-			
+
 			vote_lines = last_votes_from_person[text] + vote_lines[1:]
 
 		# store the current vote (with bandwagon alterations)
